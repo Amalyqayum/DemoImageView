adf9cb48ae968cb03000f810b609f8dd39a6df92
18016222 <amalyqayum@gmail.com>
2020-04-30 11:01:19 +0800

    Question 9

--- app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demoimageview/MainActivity.java
@@ -3,12 +3,37 @@ package sg.edu.rp.c346.demoimageview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView ivDay2;
+    ImageView ivDay3;
+    ImageView ivDay4;
+    ImageView ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Get the ImageView obj and assign to ivDay2
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        // Set the image
+        ivDay2.setImageResource(R.drawable.day2);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay3 = (ImageView) findViewById(R.id.imageView3);
+        // Set the image
+        ivDay3.setImageResource(R.drawable.day3);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay4 = (ImageView) findViewById(R.id.imageView4);
+        // Set the image
+        ivDay4.setImageResource(R.drawable.day4);
+
+        // Get the ImageView obj and assign to ivDay2
+        ivDay5 = (ImageView) findViewById(R.id.imageView5);
+        // Set the image
+        ivDay5.setImageResource(R.drawable.day5);
+
     }
 }
